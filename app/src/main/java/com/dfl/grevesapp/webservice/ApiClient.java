e8e19fe06ea3997f748c3fef6c88d37aa0751097
Diogo Loureiro <diogofialholoureiro@gmail.com>
2016-11-08 00:01:16 +0000

    Update ApiClient.java
    added condition to check if device is online

--- app/src/main/java/com/dfl/grevesapp/webservice/ApiClient.java
+++ app/src/main/java/com/dfl/grevesapp/webservice/ApiClient.java
@@ -91,6 +91,6 @@ public class ApiClient {
      */
     private static boolean isNetworkConnected(Context context) {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        return cm.getActiveNetworkInfo() != null;
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnectedOrConnecting();
     }
 }
