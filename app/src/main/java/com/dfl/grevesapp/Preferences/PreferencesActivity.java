8801c199f8586377358dc211237c4f60829215d3
Diogo Loureiro <diogofialholoureiro@gmail.com>
2016-11-10 00:14:16 +0000

    added back arrow to toolbar on preferencesactivity

--- app/src/main/java/com/dfl/grevesapp/Preferences/PreferencesActivity.java
+++ app/src/main/java/com/dfl/grevesapp/Preferences/PreferencesActivity.java
@@ -2,6 +2,10 @@ package com.dfl.grevesapp.Preferences;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.dfl.grevesapp.R;
 
 /**
  * Created by Diogo Loureiro on 09/11/2016.
@@ -12,10 +16,21 @@ public class PreferencesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
 
         getFragmentManager().beginTransaction().replace(android.R.id.content,
                 new PreferencesFragment()).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
